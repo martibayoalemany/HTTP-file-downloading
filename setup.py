fe39c90a4a4a24c7598a4e7769979962a4784619
martibayoalemany <martibayoalemany@gmail.com>
2017-06-17 18:10:38 +0200

    Store current state

--- setup.py
+++ setup.py
@@ -1,22 +1,53 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python27
+# pylint: skip-file
 from setuptools import setup, find_packages
 import unittest
+import sys
 
 
 def my_test_suite():
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('.', pattern='test_*.py')
+    test_suite = test_loader.discover('src/.', pattern='test_*.py')
     return test_suite
 
 
-setup(name='file_downloader',
-      version='1.0',
-      description='job entry test requirement',
-      author='Marti Bayo Alemany',
-      author_email='martibayoalemany@gmail.com',
-      url='http://graphai.co',
-      packages=find_packages(),
-      test_suite='setup.my_test_suite'
-      )
+if __name__ == '__main__':
 
+    if sys.version_info > (3, 0):
+        warnings.warn(
+            "The implementation was not checked against Python 3.5")
+
+    setup(name='python-file-downloader',
+          test_suite='setup.my_test_suite',
+          version='1.0.0',
+          description='python ag test',
+          url='http://graphai.co',
+          author='Marti Bayo Alemany',
+          author_email='martibayoalemany@gmail.com',
+          license='MIT',
+          #packages=find_packages(),
+          packages=['src/file_downloader'],
+          entry_points={
+              'disutils.commands': [
+                  'py_reqs = file_downloader:PyReqs'
+              ]
+
+          },
+          install_requires=[
+              'asyncio==3.4.3',
+              'autopep8==1.3.2',
+              'BeautifulSoup==3.2.1',
+              'beautifulsoup4==4.6.0',
+              'bs4==0.0.1',
+              'enum34==1.1.6',
+              'h2==2.6.2',
+              'hpack==3.0.0',
+              'html5lib==0.999999999',
+              'hyper==0.7.0',
+              'hyperframe==3.2.0',
+              'mechanize==0.3.3',
+              'pkg-resources==0.0.0',
+              'pycodestyle==2.3.1',
+              'six==1.10.0',
+              'webencodings==0.5.1', '']
+          )
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from file_downloader import downloader
-from file_downloader import Constants
-
-
-__all__=[]
